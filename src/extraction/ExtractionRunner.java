c85421527e5e4c7be991dbed604fa1d705ed6435
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-03 17:07:36 +0100

    adding ExtractionRunner

---
+++ src/extraction/ExtractionRunner.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package extraction;
+
+/**
+ * @author Ben
+ * Extracts a subset of data from DBpedia. The data extracted is a set of triples in which the supplied resource is either the subject,
+ * the predicate, or the object
+ */
+public class ExtractionRunner {
+
+	/**
+	 * @param args - accepts a single String argument, which is the URL of a resource
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
