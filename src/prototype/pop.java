6ad7c046c96dd027b866ecaf5860c6df4604309f
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-10 18:19:45 +0100

    adding ontology class consts to Pop class

--- src/prototype/pop.java
+++ src/prototype/pop.java
@@ -24,10 +24,10 @@ public class Pop {
     /**
      * Properties in the pop vocabulary
      */
-    public static final Property albumOf = model.createProperty(POP_URI + "albumOf");
-    public static final Property hasAlbum = model.createProperty(POP_URI + "hasAlbum");
     public static final Property artistOf = model.createProperty(POP_URI + "artistOf");
     public static final Property hasArtist = model.createProperty(POP_URI + "hasArtist");
+    public static final Property albumOf = model.createProperty(POP_URI + "albumOf");
+    public static final Property hasAlbum = model.createProperty(POP_URI + "hasAlbum");
     public static final Property composerOf = model.createProperty(POP_URI + "composerOf");
     public static final Property hasComposer = model.createProperty(POP_URI + "hasComposer");
     public static final Property compositionOf = model.createProperty(POP_URI + "compositionOf");
@@ -41,11 +41,15 @@ public class Pop {
     public static final Property recordLabelOf = model.createProperty(POP_URI + "recordLabelOf");
     public static final Property hasRecordLabel = model.createProperty(POP_URI + "hasRecordLabel");
     
-    
-    
     /**
      * Classes in the pop vocabulary
      */
     public static final Resource artist = model.createResource(POP_URI + "artist");
-    
+    public static final Resource album = model.createResource(POP_URI + "album");
+    public static final Resource composer = model.createResource(POP_URI + "composer");
+    public static final Resource composition = model.createResource(POP_URI + "composition");
+    public static final Resource genre = model.createResource(POP_URI + "genre");
+    public static final Resource member = model.createResource(POP_URI + "member");
+    public static final Resource producer = model.createResource(POP_URI + "producer");
+    public static final Resource recordLabel = model.createResource(POP_URI + "recordLabel");
 }
