74b26428a7e8f6dda7f45288222f71702312ef46
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-11 20:34:40 +0100

    updating ExtractionRunner

--- src/extraction/ExtractionRunner.java
+++ src/extraction/ExtractionRunner.java
@@ -3,6 +3,19 @@
  */
 package extraction;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import experiments.NsPrefixLoader;
+
 /**
  * @author Ben
  * Extracts a subset of data from DBpedia. The data extracted is a set of triples in which the supplied resource is either the subject,
@@ -10,16 +23,37 @@ package extraction;
  */
 public class ExtractionRunner {
 
-	/**
-	 * @param args - accepts a single String argument, which is the URL of a resource
-	 */
 	public static void main(String[] args) {
-		if(args.length != 1)
-			throw new IllegalArgumentException("Invalid argument to main method");
-		String url = args[0];
 		
-		System.err.println("Resource: " + url );
-
+		String fileName = "data\\objectIsABandOffset0.xml"; // <-- The name of the file the results will be saved in
+		
+		String SPARQLquery = "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>" +
+							" construct {?Subject ?Predicate ?Object.}" +
+		 					" where {?Subject ?Predicate ?Object." +
+		 					" 		 ?Object a dbpedia-owl:Band.}" +
+		 					" ORDER BY desc(?Object)";
+		
+	    Query query = QueryFactory.create(SPARQLquery);
+	    QueryExecution queryExecution = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+	    Model model = ModelFactory.createDefaultModel();
+	    queryExecution.execConstruct(model);  
+	    queryExecution.close();
+	     
+	    // load standard prefixes into the model
+	    NsPrefixLoader prefixLoader = new NsPrefixLoader(model);
+		prefixLoader.loadStandardPrefixes();
+		 
+		// Now, write the model out to a file in RDF/XML-ABBREV format:
+		try {
+			FileOutputStream outFile = new FileOutputStream(fileName);
+			model.write(outFile, "RDF/XML-ABBREV");
+			outFile.close();
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
