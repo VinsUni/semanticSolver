9f16874ba4db7fc7b67123fbab3aeca3490b9783
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-11 14:17:10 +0100

    adding getRecognisedResources and getRecognisedProperties methods

--- src/prototype/EntityRecogniser.java
+++ src/prototype/EntityRecogniser.java
@@ -3,10 +3,16 @@
  */
 package prototype;
 
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Property;
+
 /**
  * @author Ben Griffiths
  *
  */
 public interface EntityRecogniser {
-
+	public ArrayList<Resource> getRecognisedResources(String clueText);
+	public ArrayList<Property> getRecognisedProperties(String clueText);
 }
--- src/prototype/SimpleEntityRecogniser.java
+++ src/prototype/SimpleEntityRecogniser.java
@@ -3,10 +3,27 @@
  */
 package prototype;
 
+import java.util.ArrayList;
+
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+
 /**
  * @author Ben Griffiths
  *
  */
 public class SimpleEntityRecogniser implements EntityRecogniser {
+
+	@Override
+	public ArrayList<Resource> getRecognisedResources(String clueText) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Property> getRecognisedProperties(String clueText) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
