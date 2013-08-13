8ce477c187f0b7071892271ce8a39824690dd4a2
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-13 21:50:39 +0100

    first half of new test dataset merged

--- src/experiments/DatasetMerger.java
+++ src/experiments/DatasetMerger.java
@@ -3,6 +3,7 @@ package experiments;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -38,29 +39,80 @@ public class DatasetMerger {
 		String model1Url = "data\\mergedTestDatasetAug13h.xml";
 		String model2Url = "data\\dbPediaExtracts\\MusicalArtistNamesEN.xml";
 		
-		System.out.println("Loading two files to be merged...");
-		// Load the two files into the two models
-		model1 = FileManager.get().loadModel(model1Url);
-		model2 = FileManager.get().loadModel(model2Url);
+		ArrayList<String> modelsToBeMerged = new ArrayList<String>();
+		String modelFileName;
+		//modelFileName = "data\\dbPediaExtracts\\AlbumLabelsEN.xml";
+		//modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\AlbumNamesEN.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\BandLabelsEN.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\BandNamesEN.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\MusicalArtistLabelsEN.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\MusicalArtistNamesEN.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\objectIsAMusicalArtistNoAbstractsNoComments.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\objectIsAnAlbumNoAbstractsNoComments.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\objectIsARecordLabelNoAbstractsNoComments.xml";
+		modelsToBeMerged.add(modelFileName);
+		/*modelFileName = "data\\dbPediaExtracts\\predicateIsDBalbum.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBartist.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBassociatedBand.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBbandMember.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBgenre.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBppropArtist.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBproducer.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBrecordLabel.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\predicateIsDBwriter.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\subjectIsAnAlbumNoAbstractsNoCommentsWithLabels.xml";
+		modelsToBeMerged.add(modelFileName);
+		modelFileName = "data\\dbPediaExtracts\\subjectIsARecordLabelNoAbstractsNoComments.xml";
+		modelsToBeMerged.add(modelFileName); */
 		
-		System.out.println("Performing merge...");
-		Model mergedModel = model1.union(model2); // create a third model which is the union of the two models
-		
+		String modelToMergeInto = "data\\newMergedTestDataset.xml";
+		model1 = FileManager.get().loadModel(modelToMergeInto);
+		Model mergedModel = ModelFactory.createDefaultModel();
+		while(modelsToBeMerged.size() > 0) {
+			String thisModel = modelsToBeMerged.get(0);
+			System.out.println("Loading two files to be merged...");
+			// Load the two files into the two models
+			
+			model2 = FileManager.get().loadModel(thisModel);
+			
+			
+			System.out.println("Performing merge...");
+			mergedModel = model1.union(model2); // create a third model which is the union of the two models
+			modelsToBeMerged.remove(thisModel);
+			model1 = mergedModel;
+			System.out.println(thisModel + " has been merged into the dataset.");
+			
+		}
 		/* Define a prefix for one of the many namespaces used in the merged model
 		mergedModel.setNsPrefix("dbpedia-owl","http://dbpedia.org/ontology/"); */
 		
 		NsPrefixLoader prefixLoader = new NsPrefixLoader(mergedModel);
 		prefixLoader.loadStandardPrefixes();
-		
-		
 		// Now, write the model out to a file in RDF/XML-ABBREV format:
-		String fileName = "data\\mergedTestDatasetAug13i.xml";
+		String fileName = "data\\newTestDataset.xml";
 		try {
 			System.out.println("Writing result out to disk...");
 			FileOutputStream outFile = new FileOutputStream(fileName);
 			mergedModel.write(outFile, "RDF/XML-ABBREV");
 			outFile.close();
-			System.out.println("Operation complete.");
+			System.out.println("Operation complete");
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
@@ -68,5 +120,7 @@ public class DatasetMerger {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+	
+		System.out.println("All models now merged");
 	}
 }
