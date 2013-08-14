3e229cb58989d37176e181778a8c035bf810460f
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-14 20:31:06 +0100

    adding QueryRunner class

---
+++ src/prototype/QueryRunner.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package prototype;
+
+/**
+ * @author Ben Griffiths
+ * 
+ */
+public class QueryRunner implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
