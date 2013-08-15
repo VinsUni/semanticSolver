26702f20194d72ebbb34fd1c9e39d051d9f882a2
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-15 21:31:53 +0100

    Configuring logger

--- src/app/SolverRunnerImpl.java
+++ src/app/SolverRunnerImpl.java
@@ -1,5 +1,9 @@
 package app;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import framework.UserInterface;
 
 /**
@@ -12,6 +16,10 @@ public class SolverRunnerImpl {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/* log4j logging configuration */
+		Logger.getRootLogger().setLevel(Level.INFO);
+		PropertyConfigurator.configure("log4j.properties");
+		
 		UserInterface ui = new UserInterfaceImpl();
 		ui.createAndShow();
 	}
