963bb14346cad8fdb61768da965787c0af7b170b
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-15 21:56:00 +0100

    adding SemanticSolverImpl

---
+++ src/app/SemanticSolverImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package app;
+
+import framework.Clue;
+import framework.SemanticSolver;
+
+/**
+ * @author Ben Griffiths
+ *
+ */
+public class SemanticSolverImpl implements SemanticSolver {
+
+	@Override
+	public void solve(Clue clue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
