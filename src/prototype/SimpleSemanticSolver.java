82f0ee2c0c1b67592beb5a8d9dc80b38fc1e81ac
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-16 18:39:11 +0100

    adding Controller to local prototype

---
+++ src/prototype/SimpleSemanticSolver.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package prototype;
+
+import framework.Clue;
+import framework.SemanticSolver;
+
+/**
+ * @author Ben Griffiths
+ *
+ */
+public class SimpleSemanticSolver implements SemanticSolver {
+
+	@Override
+	public void solve(Clue clue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
