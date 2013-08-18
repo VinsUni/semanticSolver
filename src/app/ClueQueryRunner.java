1dcab694c9a318a83d9e07f5472187b6117b8a5e
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-18 16:20:44 +0100

    adding ClueQueryRunner

---
+++ src/app/ClueQueryRunner.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package app;
+
+/**
+ * @author Ben Griffiths
+ *
+ */
+public class ClueQueryRunner implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
