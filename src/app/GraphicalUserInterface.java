dd5642208562e86a3fdd111fc5256f3fba4d65be
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-18 22:00:01 +0100

    adding GraphicalUserInterface class

---
+++ src/app/GraphicalUserInterface.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package app;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Ben Griffiths
+ *
+ */
+@SuppressWarnings("serial")
+public class GraphicalUserInterface extends JFrame {
+	
+}
