71ada45bc8d0dd8b17fbeeac60602ab7ce36dccf
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-18 22:04:12 +0100

    adding skeleton GUI

--- src/app/GraphicalUserInterface.java
+++ src/app/GraphicalUserInterface.java
@@ -4,12 +4,26 @@
 package app;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import framework.UserInterface;
 
 /**
  * @author Ben Griffiths
  *
  */
 @SuppressWarnings("serial")
-public class GraphicalUserInterface extends JFrame {
+public class GraphicalUserInterface extends JFrame implements UserInterface {
+	private JPanel contentPane;
+	@Override
+	public void createAndShow() {
+		this.setTitle("Semantic Crossword Solver");
+		
+		contentPane = new JPanel();
+		this.setContentPane(contentPane);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.setVisible(true);
+		
+	}
 	
 }
