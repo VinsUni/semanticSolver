77eeb43fec907ded8b1cc2cb94c3bdf24c691cd1
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-23 19:12:27 +0100

    adding RecognisedResource class to wrap found resources

---
+++ src/app/RecognisedResource.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package app;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Ben Griffiths
+ *
+ */
+public class RecognisedResource {
+	@Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private String uri;
+    @Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private String resourceLabel;
+    @Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private String typeLabel;
+
+    public RecognisedResource(String uri, String resourceLabel, String typeLabel) {
+           this.setUri(uri);
+           this.setResourceLabel(resourceLabel);
+           this.setTypeLabel(typeLabel);
+    }
+}
