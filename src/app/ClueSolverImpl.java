97ce321c497361c82f4e0a7663741cf73011750c
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-25 19:23:59 +0100

    removing obsolete methods from ClueSolver interface

--- src/app/ClueSolverImpl.java
+++ src/app/ClueSolverImpl.java
@@ -46,30 +46,7 @@ public class ClueSolverImpl implements ClueSolver {
 		
 		return acceptedSolutions;
 	}
-	
-	/**
-	 * For now, it just returns all of the proposedSolutions and sets the first one as the best solution,
-	 * but it will need to screen out those that do not solve the clue
-	 * (e.g. due to being the wrong number of words/wrong length 
-	 */
-	@Override
-	public ArrayList<String> getCandidateSolutions(Clue clue, ArrayList<String> proposedSolutions) {
-		proposedSolutions = this.filterByLanguage(proposedSolutions);
-		ArrayList<String> solutions = new ArrayList<String>();
-		for(String proposedSolution : proposedSolutions) {
-			Solution parsedSolution = new SolutionImpl(proposedSolution);
-			String solutionText = parsedSolution.getSolutionText();
-			if(!solutions.contains(solutionText) && isWellFormedSolution(solutionText) && clue.matchesStructure(parsedSolution))
-				solutions.add(solutionText);
-		}
-		
-		if(proposedSolutions.size() > 0)
-			this.setBestSolutionText(proposedSolutions.get(0));
-		
-		return solutions;
-	}
-	
-	
+
 	/**
 	 * PRETTY MUCH DUPLICATED IN THE filterByLanguage METHOD BELOW
 	 * @param proposedSolutions
@@ -87,26 +64,6 @@ public class ClueSolverImpl implements ClueSolver {
 						continue; // non-English language, so filter it out
 			}
 			filteredSolutions.add(thisSolution);
-		}
-		return filteredSolutions;
-	}
-
-	
-	
-	/*
-	 * THIS CODE IS DUPLICATED IN THE SIMPLEENTITYRECOGNISER CLASS - REFACTOR IT OUT SOMEWHERE?
-	 */
-	private ArrayList<String> filterByLanguage(ArrayList<String> proposedSolutions) {
-		ArrayList<String> filteredSolutions = new ArrayList<String>();
-		for(int i = 0; i < proposedSolutions.size(); i++) {
-			String solutionText = proposedSolutions.get(i);
-			int positionOfLanguageTag = solutionText.length() - LANGUAGE_TAG_LENGTH;
-			if(solutionText.length() > LANGUAGE_TAG_LENGTH) {
-				if(solutionText.substring(positionOfLanguageTag, positionOfLanguageTag + 1).equals(LANGUAGE_TAG) 
-					&& !solutionText.substring(positionOfLanguageTag + 1, solutionText.length()).equals("en"))
-						continue; // non-English language, so filter it out
-			}
-			filteredSolutions.add(solutionText);
 		}
 		return filteredSolutions;
 	}
--- src/framework/ClueSolver.java
+++ src/framework/ClueSolver.java
@@ -18,7 +18,7 @@ public interface ClueSolver {
 	 * @param proposedSolutions - an ArrayList of Strings resulting from a query of the model for an answer to the clue
 	 * @return a subset of the list of proposedSolutions, each member of which matches the requirements of the clue
 	 */
-	public ArrayList<String> getCandidateSolutions(Clue clue, ArrayList<String> proposedSolutions);
+
 	public String getBestSolution(Clue clue);
 	public ArrayList<Solution> getSolutions(Clue clue, ArrayList<Solution> proposedSolutions) throws NoSolutionsException;
 }
