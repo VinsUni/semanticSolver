8177f409ace53147e83147e2816623f424f3dc81
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-25 15:55:57 +0100

    commenting methods

--- src/app/CandidateSelector.java
+++ src/app/CandidateSelector.java
@@ -17,20 +17,37 @@ import framework.remotePrototype.Pop;
  */
 public class CandidateSelector extends SimpleSelector {
 	
+	/**
+	 * Default constructor. Constructs a new CandidateSelector with the specified arguments
+	 * @param subject - the subject to be matched by the CandidateSelector
+	 * @param predicate - the predicate to be matched by the CandidateSelector
+	 * @param object - the object to be matched by the CandidateSelector
+	 */
 	public CandidateSelector(Resource subject, Property predicate, RDFNode object) {
 		super(subject, predicate, object);
 	}
 	
-	
+	/**
+	 * selects - returns true if the statement should be selected by this CandidateSelector, otherwise false.
+	 * See com.hp.hpl.jena.rdf.model.SimpleSelecto;.
+	 * @argument statement - the statement to be tested
+	 * @return false for all statements whose predicate is a sub-property of pop:relationalProperty, and true for all others
+	 */
 	@Override
-	public boolean selects(Statement s) {
-		Property predicate = s.getPredicate();
+	public boolean selects(Statement statement) {
+		Property predicate = statement.getPredicate();
 
 		if(predicate.equals(Pop.relationalProperty))
 			return false;
 		return true;
 	}
 	
+	/**
+	 * isSimple - used by the Jena framework to ascertain whether or not the selects statement has been overriden for this 
+	 * implementation of the Selector interface. See com.hp.hpl.jena.rdf.model.SimpleSelector.
+	 * @return false, since the selects method has been overwritten.
+	 * 
+	 */
 	@Override
 	public boolean isSimple() {
 		return false;
