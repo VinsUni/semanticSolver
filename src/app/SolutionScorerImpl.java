ff6e7fe24f0a745369bb32aabccd98319d8d0c2a
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-26 16:21:10 +0100

    implemented scoring of solutions

--- src/app/SolutionScorerImpl.java
+++ src/app/SolutionScorerImpl.java
@@ -40,12 +40,21 @@ public class SolutionScorerImpl implements SolutionScorer {
 	private final int LANGUAGE_TAG_LENGTH = 3;
 	private final String LANGUAGE_TAG = "@";
 	
+	private final String DBPEDIA_PROPERTY_PREFIX_DECLARATION = "PREFIX dbpprop: <http://dbpedia.org/property/>"; // the 'old' property ontology
+	private final String RDFS_PREFIX_DECLARATION = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>";
+	private final String RDF_PREFIX_DECLARATION = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
+	
+	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private Clue clue;
+	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private Solution solution;
+	
 	@Override
 	public double score(Solution solution) {
+		this.setSolution(solution);
 		
 		double distanceBetweenClueAndSolution = distance(solution.getSolutionResource(), solution.getClueResource());
 		
 		ArrayList<Resource> solutionTypes = this.getSolutionTypes(solution);
+		ArrayList<Resource> solutionProperties = this.getSolutionProperties(solution);
 		
 		System.out.println(); //DEBUGGING **********************************
 		System.out.println("Recognised the following types in the solutionResource " + solution.getSolutionResource().getURI() + ":"); //DEBUGGING **********************************
@@ -53,18 +62,26 @@ public class SolutionScorerImpl implements SolutionScorer {
 			System.out.println(solutionType.getURI()); //DEBUGGING **********************************
 		System.out.println(); //DEBUGGING **********************************
 		
-		double distanceBetweenClueTypesAndSolution = 1.0;
+		System.out.println(); //DEBUGGING **********************************
+		System.out.println("Recognised the following properties of the solutionResource " + solution.getSolutionResource().getURI() + ":"); //DEBUGGING **********************************
+		for(Resource solutionProperty : solutionProperties) //DEBUGGING **********************************
+			System.out.println(solutionProperty.getURI()); //DEBUGGING **********************************
+		System.out.println(); //DEBUGGING **********************************
 		
-		return distanceBetweenClueAndSolution * distanceBetweenClueTypesAndSolution;
+		
+		
+		double distanceBetweenClueFragmentsAndSolution = distance(solution.getSolutionResource(), solutionTypes, solutionProperties);
+		
+		return distanceBetweenClueAndSolution * distanceBetweenClueFragmentsAndSolution;
 	}
 	
 	private ArrayList<Resource> getSolutionTypes(Solution solution) {
 		ArrayList<Resource> solutionTypes = new ArrayList<Resource>();
 
 		InfModel infModel = solution.getInfModel();
-		Clue clue = solution.getClue();
+		this.setClue(solution.getClue());
 		
-		ArrayList<String> clueFragments = clue.getClueFragments();
+		ArrayList<String> clueFragments = this.getClue().getClueFragments();
 		
 		/* Find the types of the clueResource */
 		Selector selector = new SimpleSelector(solution.getSolutionResource(), RDF.type, (RDFNode) null);
@@ -93,8 +110,16 @@ public class SolutionScorerImpl implements SolutionScorer {
 					solutionTypes.add(thisType);
 			}
 		}
+		return solutionTypes;
+	}
+	
+	private ArrayList<Resource> getSolutionProperties(Solution solution) {
+		ArrayList<Resource> solutionProperties = new ArrayList<Resource>();
+
+		InfModel infModel = solution.getInfModel();
+		Clue clue = solution.getClue();
 		
-		/* Find any properties with labels matching the clue fragments and add these too */
+		ArrayList<String> clueFragments = clue.getClueFragments();
 		
 		Resource clueResource = solution.getClueResource();
 		Selector predicateSelector = new SimpleSelector(solution.getSolutionResource(), null, (RDFNode) clueResource);
@@ -120,13 +145,10 @@ public class SolutionScorerImpl implements SolutionScorer {
 				System.out.println("Found label: " + thisPredicateLabel); // DEBUGGING *****************************************
 				
 				if(clueFragments.contains(toProperCase(thisPredicateLabel)))
-					solutionTypes.add(thisPredicate);
+					solutionProperties.add(thisPredicate);
 			}
 		}
-		
-		
-		
-		return solutionTypes;
+		return solutionProperties;
 	}
 	
 	/*
@@ -171,6 +193,56 @@ public class SolutionScorerImpl implements SolutionScorer {
 		
 		return distance;
 	}
+	
+	private double distance(Resource solutionResource, ArrayList<Resource> recognisedSolutionTypes, 
+			ArrayList<Resource> recognisedSolutionProperties) {
+		
+		if(recognisedSolutionTypes.size() == 0 && recognisedSolutionProperties.size() == 0)
+			return 1.0;
+		
+		String solutionResourceUri = solutionResource.getURI();
+		String clueResourceUri = this.getSolution().getClueResource().getURI();
+		
+		String queryBuffer = "";
+		
+		for(int i = 0; i < recognisedSolutionTypes.size(); i++) {
+			if(i > 0)
+				queryBuffer += " UNION";
+			
+			String typeUri = recognisedSolutionTypes.get(i).getURI();
+			queryBuffer += " {<" + solutionResourceUri + "> rdf:type <" + typeUri + ">." + " }";
+		}
+		
+		if(recognisedSolutionTypes.size() > 0 && recognisedSolutionProperties.size() > 0)
+			queryBuffer += " UNION";
+		
+		for(int i = 0; i < recognisedSolutionProperties.size(); i++) {
+			if(i > 0)
+				queryBuffer += " UNION";
+			
+			String predicateUri = recognisedSolutionProperties.get(i).getURI();
+			queryBuffer += " {<" + solutionResourceUri + "> <" + predicateUri + "> <" + clueResourceUri + ">}" +
+						" UNION" +
+						" {<" + clueResourceUri + "> <" + predicateUri + "> <" + solutionResourceUri + ">}";
+		}
+		
+		
+		String sparqlQueryStart = this.RDF_PREFIX_DECLARATION +
+							" select (count(*) as ?count) where {";
+		
+		String sparqlQueryEnd = " }";
+		
+		String sparqlQuery = sparqlQueryStart + queryBuffer + sparqlQueryEnd;
+		
+		double numberOfLinks = this.executeCountQuery(sparqlQuery);
+		
+		System.err.println("Second count query for solutionResource " + solutionResourceUri + " - " + sparqlQuery + " - has result: " +
+				numberOfLinks); // DEBUGGING ***********************************************************************
+		
+		double distance = (1.0 / (1.0 + numberOfLinks));
+		
+		return distance;
+	}
 
 	private double countLinks(Resource firstResource, Resource secondResource) {
 		String firstResourceUri = firstResource.getURI();
@@ -195,6 +267,22 @@ public class SolutionScorerImpl implements SolutionScorer {
 
 		queryExecution.close();
 		return numberOfLinks;
+	}
+	
+	private double executeCountQuery(String countQuery) {
+		Query query = QueryFactory.create(countQuery);
+		QueryExecution queryExecution = QueryExecutionFactory.sparqlService(this.ENDPOINT_URI, query);
+		ResultSet resultSet = queryExecution.execSelect();
+        QuerySolution querySolution = resultSet.nextSolution();
+        
+        Literal numberOfLinksAsLiteral = querySolution.getLiteral("?count");
+        double numberOfLinks = numberOfLinksAsLiteral.getDouble();
+        
+        System.out.println("Number of links found: " + numberOfLinks); // DEBUGGING ****************
+
+		queryExecution.close();
+		return numberOfLinks;
+		
 	}
 	
 	private double countTypeLinks(Resource solutionResource, ArrayList<Resource> clueTypes) {
