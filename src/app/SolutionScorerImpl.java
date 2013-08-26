8147407ccd7712b90ec7f3ce2984da2ea621be01
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-26 11:35:50 +0100

    adding implementation of SolutionScorer

---
+++ src/app/SolutionScorerImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package app;
+
+import framework.Clue;
+import framework.SolutionScorer;
+
+/**
+ * @author Ben Griffiths
+ *
+ */
+public class SolutionScorerImpl implements SolutionScorer {
+
+	@Override
+	public double score(Clue clue) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
