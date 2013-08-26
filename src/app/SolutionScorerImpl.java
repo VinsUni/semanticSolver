8ac45cddc02709db040bf101e8764b8efd01e1cc
Ben Griffiths <benstephengriffiths@gmail.com>
2013-08-26 15:09:19 +0100

    adding getSolutionTypes method (needs renaming)

--- src/app/SolutionScorerImpl.java
+++ src/app/SolutionScorerImpl.java
@@ -14,9 +14,14 @@ import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Selector;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -32,34 +37,131 @@ import framework.SolutionScorer;
  */
 public class SolutionScorerImpl implements SolutionScorer {
 	private final String ENDPOINT_URI = "http://dbpedia.org/sparql";
+	private final int LANGUAGE_TAG_LENGTH = 3;
+	private final String LANGUAGE_TAG = "@";
 	
 	@Override
 	public double score(Solution solution) {
 		
 		double distanceBetweenClueAndSolution = distance(solution.getSolutionResource(), solution.getClueResource());
 		
-		ArrayList<Resource> clueTypes = this.getClueTypes(solution);
+		ArrayList<Resource> solutionTypes = this.getSolutionTypes(solution);
 		
-		
+		System.out.println(); //DEBUGGING **********************************
+		System.out.println("Recognised the following types in the solutionResource " + solution.getSolutionResource().getURI() + ":"); //DEBUGGING **********************************
+		for(Resource solutionType : solutionTypes) //DEBUGGING **********************************
+			System.out.println(solutionType.getURI()); //DEBUGGING **********************************
+		System.out.println(); //DEBUGGING **********************************
 		
 		double distanceBetweenClueTypesAndSolution = 1.0;
 		
 		return distanceBetweenClueAndSolution * distanceBetweenClueTypesAndSolution;
 	}
 	
-	private ArrayList<Resource> getClueTypes(Solution solution) {
-		ArrayList clueTypes = new ArrayList<Resource>();
-		
+	private ArrayList<Resource> getSolutionTypes(Solution solution) {
+		ArrayList<Resource> solutionTypes = new ArrayList<Resource>();
+
 		InfModel infModel = solution.getInfModel();
 		Clue clue = solution.getClue();
 		
-		//ArrayList<String> clueFragments = 
+		ArrayList<String> clueFragments = clue.getClueFragments();
 		
-		//StmtIterator typeStatements = 
+		/* Find the types of the clueResource */
+		Selector selector = new SimpleSelector(solution.getSolutionResource(), RDF.type, (RDFNode) null);
 		
-		// TODO Auto-generated method stub
-		return null;
+		StmtIterator solutionTypeStatements = infModel.listStatements(selector);
+		
+		/* add labels to the types */
+		while(solutionTypeStatements.hasNext()) {
+			Statement thisStatement = solutionTypeStatements.nextStatement();
+			
+			System.err.println(thisStatement.toString()); // DEBUGGING ***********************************************
+			
+			Resource thisType = thisStatement.getObject().asResource();
+			
+			StmtIterator typeLabels = thisType.listProperties(RDFS.label);
+			
+			while(typeLabels.hasNext()) {
+				Statement thisTypeLabelStatement = typeLabels.nextStatement();
+				
+				String thisLabel = thisTypeLabelStatement.getString();
+				thisLabel = stripLanguageTag(thisLabel);
+				
+				System.out.println("Found label: " + thisLabel); // DEBUGGING *****************************************
+				
+				if(clueFragments.contains(toProperCase(thisLabel)))
+					solutionTypes.add(thisType);
+			}
+		}
+		
+		/* Find any properties with labels matching the clue fragments and add these too */
+		
+		Resource clueResource = solution.getClueResource();
+		Selector predicateSelector = new SimpleSelector(solution.getSolutionResource(), null, (RDFNode) clueResource);
+		
+		StmtIterator solutionPropertyStatements = infModel.listStatements(predicateSelector);
+		
+		/* add labels to the types */
+		while(solutionPropertyStatements.hasNext()) {
+			Statement thisStatement = solutionPropertyStatements.nextStatement();
+			
+			System.err.println(thisStatement.toString()); // DEBUGGING ***********************************************
+			
+			Resource thisPredicate = thisStatement.getPredicate().asResource();
+			
+			StmtIterator predicateLabels = thisPredicate.listProperties(RDFS.label);
+			
+			while(predicateLabels.hasNext()) {
+				Statement thisPredicateLabelStatement = predicateLabels.nextStatement();
+				
+				String thisPredicateLabel = thisPredicateLabelStatement.getString();
+				thisPredicateLabel = stripLanguageTag(thisPredicateLabel);
+				
+				System.out.println("Found label: " + thisPredicateLabel); // DEBUGGING *****************************************
+				
+				if(clueFragments.contains(toProperCase(thisPredicateLabel)))
+					solutionTypes.add(thisPredicate);
+			}
+		}
+		
+		
+		
+		return solutionTypes;
 	}
+	
+	/*
+	 * DUPLICATED IN CLUEIMPL CLASS
+	 */
+	private String toProperCase(String thisWord) {
+		String thisWordInProperCase = thisWord.substring(0, 1).toUpperCase();
+		if(thisWord.length() > 1) {
+			int index = 1; // start at the second letter of the word
+			while(index < thisWord.length()) {
+				String nextCharacter = thisWord.substring(index, index + 1);
+				thisWordInProperCase += nextCharacter;
+				if((nextCharacter.equals(" ")) && (index < (thisWord.length() - 1))) {
+					 index++; // the next character needs to be capitalised
+					 nextCharacter = thisWord.substring(index, index + 1);
+					 thisWordInProperCase += nextCharacter.toUpperCase();
+				}
+				index++;
+			}
+		}
+		return thisWordInProperCase;
+	}
+	
+	/*
+	 * THIS CODE IS DUPLICATED IN THE SIMPLEENTITYRECOGNISER CLASS - REFACTOR IT OUT SOMEWHERE?
+	 */
+	private String stripLanguageTag(String solutionText) {
+		int positionOfLanguageTag = solutionText.length() - LANGUAGE_TAG_LENGTH;
+		if(solutionText.length() > LANGUAGE_TAG_LENGTH) {
+			if(solutionText.substring(positionOfLanguageTag, positionOfLanguageTag + 1).equals(LANGUAGE_TAG))
+				return solutionText.substring(0, positionOfLanguageTag);
+		}
+		return solutionText;
+	}
+	
 
 	private double distance(Resource firstResource, Resource secondResource) {
 		
