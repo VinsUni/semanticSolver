b26e8a0e960ab3f1fd4155585b3d88f4bef4cbcc
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-10 09:44:27 +0100

    ModelLoader now returns a Model rather than InfModel in order to conserve heap space

--- src/app/ModelLoader.java
+++ src/app/ModelLoader.java
@@ -10,10 +10,8 @@ import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
-import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.reasoner.ReasonerRegistry;
+
 import com.hp.hpl.jena.shared.JenaException;
 import com.hp.hpl.jena.util.FileManager;
 
@@ -29,7 +27,7 @@ import lombok.Setter;
  */
 public class ModelLoader {
 	private static Logger log = Logger.getLogger(SemanticSolverImpl.class);
-	@Setter(AccessLevel.PRIVATE) private static InfModel model;
+	@Setter(AccessLevel.PRIVATE) private static Model model;
 	@Setter(AccessLevel.PRIVATE) private static Model knowledgeBase;
 	@Setter(AccessLevel.PRIVATE) private static Map<String, Boolean> commonClueFragments;
 	private static final String COMMON_CLUE_FRAGMENTS_FILE_LOCATION = "commonClueFragments.txt";
@@ -39,17 +37,14 @@ public class ModelLoader {
 	}
 	
 	/**
-	 * getModel - when first called, this method instantiates an InfModel, by loading the Pop ontology from a file and binding it to
-	 * an OWL Mini reasoner. It also loads a set of standard namespace prefixes into the InfModel, from another file, and then returns
-	 * the model. The InfModel member is treated as a Singleton and thus subsequent calls to the method result in the InfModel simply being
-	 * returned. 
-	 * @return - the unique static instance of an InfModel that is a member of the ModelLoader class
+	 * getModel - when first called, this method instantiates a Model by loading the Pop ontology from a file. It also loads a set of 
+	 * standard namespace prefixes into the Model, from another file, and then returns the model. The Model member is treated as a 
+	 * Singleton and thus subsequent calls to the method result in the Model simply being returned. 
+	 * @return - the unique static instance of Model that is a member of the ModelLoader class
 	 */
-	public static InfModel getModel() {
+	public static Model getModel() {
 		if(model == null) {
-			Model baseModel = FileManager.get().loadModel(Pop.LOCAL_VOCAB_URI);
-			/* Create an inference model using the ontology and an OWL Mini reasoner */
-			setModel(ModelFactory.createInfModel(ReasonerRegistry.getOWLMiniReasoner(), baseModel));
+			setModel(FileManager.get().loadModel(Pop.LOCAL_VOCAB_URI));
 			/* load standard prefixes into the model */
 			NsPrefixLoader prefixLoader = new NsPrefixLoader(model);
 			prefixLoader.loadStandardPrefixes();
