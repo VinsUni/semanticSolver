0a3a4bbc4f09da95606b8e5a15c97ccb5c867fce
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 19:55:08 +0100

    commenting GraphicalUIRunner class

--- src/app/GraphicalUIRunner.java
+++ src/app/GraphicalUIRunner.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package app;
 
 import javax.swing.SwingUtilities;
@@ -11,17 +8,18 @@ import org.apache.log4j.PropertyConfigurator;
 
 import framework.UserInterface;
 
-
 /**
  * @author Ben Griffiths
- *
+ * GraphicalUIRunner
+ * Contains the application's main method, which configures the logging system and then creates the graphical user interface
+ * on the Event Dispatch Thread
  */
 public class GraphicalUIRunner {
 	public static void main(String[] args) {
 		/* log4j logging configuration */
 		Logger.getRootLogger().setLevel(Level.INFO);
 		PropertyConfigurator.configure("log4j.properties");
-		
+		/* Create the graphical user interface on the Event Dispatch thread */
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				UserInterface ui = new GraphicalUserInterface();
