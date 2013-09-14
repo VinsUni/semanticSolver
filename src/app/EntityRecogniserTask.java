d04a26ed967832c0aa26a387e1b1a1061c35be64
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 19:38:56 +0100

    beginning to tidy and comment EntityRecogniserTask class

--- src/app/EntityRecogniserTask.java
+++ src/app/EntityRecogniserTask.java
@@ -16,7 +16,6 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
@@ -30,75 +29,37 @@ import framework.Clue;
 
 /**
  * @author Ben Griffiths
- *
+ * EntityRecogniserTask
+ * The EntityRecogniserTask queries DBpedia's SPARQL endpoint in order to compile a list of URIS of resources whose labels match fragments
+ * of the clue text of the clue with which it is initialised.
+ * @extends javax.swing.SwingWorker
  */
 public class EntityRecogniserTask extends SwingWorker<ArrayList<String>, Void> {
 	private static Logger log = Logger.getLogger(EntityRecogniserTask.class);
 	private final String LANG = "@en";
-	private final String ENDPOINT_URI = "http://dbpedia-live.openlinksw.com/sparql"; // http://dbpedia.org/sparql // DUPLICATED IN QUERYIMPL
-	private final String RDFS_PREFIX_DECLARATION = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"; // DUPLICATED IN QUERYIMPL
+	private final String ENDPOINT_URI = "http://dbpedia-live.openlinksw.com/sparql";
+	private final String RDFS_PREFIX_DECLARATION = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>";
 	private final String DBPPROP_PREFIX_DECLARATION = "PREFIX dbpprop: <http://dbpedia.org/property/>";
 	private final String DB_OWL_PREFIX_DECLARATION = "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>";
-	private final String RDF_PREFIX_DECLARATION = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
 	private final String XPATH_FUNCTIONS_PREFIX_DECLARATION = "PREFIX fn: <http://www.w3.org/2005/xpath-functions#>";
 	private final String FOAF_PREFIX_DECLARATION = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>";
 	private final int RESULT_LIMIT = 200;
 	private final int FITB_RESULT_LIMIT = 100;
 	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private Clue clue;
-	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private StmtIterator statementsIterator; // used to iterate over the statements in my local ontology
+	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private StmtIterator statementsIterator;
 	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private ResIterator propertiesIterator;
-
-	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) ArrayList<String> recognisedResourceUris;
-	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) Map<String, Boolean> commonClueFragments;
+	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private ArrayList<String> recognisedResourceUris;
+	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private Map<String, Boolean> commonClueFragments;
 	
-
-	/* This constructor will be used to instantiate a task that recognises entities in a clue */
-	public EntityRecogniserTask(Clue clue) {
-		super(); // call SwingWorker Default constructor
-		this.setClue(clue);
-		this.setRecognisedResourceUris(new ArrayList<String>());
-		this.setCommonClueFragments(ModelLoader.getCommonClueFragments());
-	}
-    
-	/*
-     * Main task. Executed in background thread. 
-     */
-    @Override
-    public ArrayList<String> doInBackground() {
-        int progress = 0;
-        this.setProgress(progress); // Initialise progress property of SwingWorker
-        
-        
-        int combinedLengthOfQueries = this.getClue().getClueFragments().size();
-        int taskLength = (100 / combinedLengthOfQueries);
-        
-        for(String clueFragment : this.getClue().getClueFragments()) {
-        	try {
-        		if(this.getClue().isFillInTheBlank())
-        			this.extractFITBEntities(clueFragment);
-        		else this.extractEntities(clueFragment);
-        	}
-        	catch (QueryExceptionHTTP e) {
-        		log.debug("DBpedia connection dropped. Entity recognition for clue fragment " + clueFragment + " failed");
-        		log.debug(e.getResponseMessage());
-        	}
-        	progress += taskLength;
-            this.setProgress(progress); // one query has been completed
-        }
-        return this.getRecognisedResourceUris();
-    }
-    
-    /*
-     * Executed on EDT
-     */
-    @Override
-    public void done() {
-    	this.setProgress(100);
-    }
-    
-    private void extractEntities(String clueFragment) throws QueryExceptionHTTP {
+	/**
+	 * extractEntities - constructs a list of URIs of resources in the DBpedia knowledge base whose labels match exactly
+	 * the given clue fragment
+	 * @param clueFragment - the fragment of clue text with which to try to find resources on DBpedia with matching labels
+	 * @throws com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP - if any of the generated SPARQL queries throw this exception
+	 */
+	private void extractEntities(String clueFragment) throws QueryExceptionHTTP {
     	if(this.getCommonClueFragments().containsKey(clueFragment.toLowerCase()))
-    		return;	
+    		return;	// do not construct models around the most commonly occurring English words, as defined in the commonClueFragments list
 	     String wrappedClueFragment = "\"" + clueFragment + "\"" + LANG; // wrap with escaped quotes and append a language tag
 	
 	     String SPARQLquery = RDFS_PREFIX_DECLARATION + " " +
@@ -175,7 +136,7 @@ public class EntityRecogniserTask extends SwingWorker<ArrayList<String>, Void> {
 	
 	private void extractFITBEntities(String clueFragment) throws QueryExceptionHTTP {
 		if(this.getCommonClueFragments().containsKey(clueFragment.toLowerCase()))
-    		return;
+    		return; // do not construct models around the most commonly occurring English words, as defined in the commonClueFragments list
 		
 	    String wrappedClueFragment = "'\"" + clueFragment + "\"'";
 	    
@@ -273,4 +234,49 @@ public class EntityRecogniserTask extends SwingWorker<ArrayList<String>, Void> {
 	             throw e;
 	    }
 	}
+
+	/* This constructor will be used to instantiate a task that recognises entities in a clue */
+	public EntityRecogniserTask(Clue clue) {
+		super(); // call SwingWorker Default constructor
+		this.setClue(clue);
+		this.setRecognisedResourceUris(new ArrayList<String>());
+		this.setCommonClueFragments(ModelLoader.getCommonClueFragments());
+	}
+    
+	/**
+	 * 
+	 * @override javax.swing.SwingWorker.doInBackground
+	 */
+    @Override
+    public ArrayList<String> doInBackground() {
+        int progress = 0;
+        this.setProgress(progress); // Initialise progress property of SwingWorker
+        
+        
+        int combinedLengthOfQueries = this.getClue().getClueFragments().size();
+        int taskLength = (100 / combinedLengthOfQueries);
+        
+        for(String clueFragment : this.getClue().getClueFragments()) {
+        	try {
+        		if(this.getClue().isFillInTheBlank())
+        			this.extractFITBEntities(clueFragment);
+        		else this.extractEntities(clueFragment);
+        	}
+        	catch (QueryExceptionHTTP e) {
+        		log.debug("DBpedia connection dropped. Entity recognition for clue fragment " + clueFragment + " failed");
+        		log.debug(e.getResponseMessage());
+        	}
+        	progress += taskLength;
+            this.setProgress(progress); // one query has been completed
+        }
+        return this.getRecognisedResourceUris();
+    }
+    
+    /**
+     * @override javax.swing.SwingWorker.done
+     */
+    @Override
+    public void done() {
+    	this.setProgress(100);
+    }
 }
