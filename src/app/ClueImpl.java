56d3cc362c08832cda58b0f915e0ea149ca0c087
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 17:26:47 +0100

    tidying and commenting ClueImpl class

--- src/app/ClueImpl.java
+++ src/app/ClueImpl.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package app;
 
 import java.util.ArrayList;
@@ -19,69 +16,28 @@ import lombok.Setter;
 
 /**
  * @author Ben Griffiths
+ * ClueImpl
  * Represents a clue
+ * @implements framework.Clue
  */
 public class ClueImpl implements Clue {
 	private static Logger log = Logger.getLogger(ClueImpl.class);
 	private final String APOSTROPHE_S_SEQUENCE = "'s"; // if present in a clue, requires further special transformation
 	private final String S_APOSTROPHE_SEQUENCE = "s'"; // if present in a clue, requires further special transformation
-	private final String FILL_IN_THE_BLANK_MARKER = "_";
-	private final String[] PUNCTUATION = {":", ";", ",", ".", "-"};
-	
+	private final String FILL_IN_THE_BLANK_MARKER = "_"; // the presence of this marker in the text of a clue signifies a FITB-type clue
+	private final String[] PUNCTUATION = {":", ";", ",", ".", "-"}; // common punctuation marks used in the text of clues
 	@Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private ArrayList<String> clueFragments;
 	@Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private String sourceClue;
 	@Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private int[] SolutionStructure;
-	@Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private boolean fillInTheBlank; // true if the clue is a 'Fill in the blank' style clue
+	@Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE) private boolean fillInTheBlank;
 
-	/**
-	 * Constructor - constructs a clue from a String representing the clue text and an int[] representing the solution structure
-	 * @param clueText - the text of the clue
-	 * @param solutionStructure - an array of integers representing the structure of the solution, with each element of the array
-	 * representing a word, and the value of the element representing the number of letters in that array
-	 * @throws InvalidClueException - if either the clue text or the structure of the solution is invalid
-	 */
-	public ClueImpl(String clueText, int[] solutionStructure) throws InvalidClueException {
-		if(clueText == null || clueText.length() == 0)
-			throw new InvalidClueException("Empty clue");
-		if(solutionStructure == null || solutionStructure.length == 0)
-			throw new InvalidClueException("Invalid specification of solution structure");
-		for(int i = 0; i < solutionStructure.length; i++) {
-			if(solutionStructure[i] < 1)
-				throw new InvalidClueException("Invalid specification of solution structure");
-		}
-		this.setSolutionStructure(solutionStructure);
-		this.setClueFragments(new ArrayList<String>());
-		
-		
-		if(clueText.contains(FILL_IN_THE_BLANK_MARKER))
-			this.setFillInTheBlank(true);
-		
-		if(this.isFillInTheBlank() && imbalancedFITBMarkers(clueText))
-			throw new InvalidClueException("The numbed of underscores in the clue text doesn't " +
-											"match the number of words in the solution");
-		
-		this.setSourceClue(clueText);
-		this.parseClueText(clueText);
-		
-		
-		/* Logging */
-		log.debug("Clue text = " + this.getSourceClue());
-		log.debug("Clue fragments generated:");
-		for(String f : this.getClueFragments())
-			log.debug(f);
-	}
-	
-	
-	
 	/**
 	 * parseClueText - removes segments surrounded by double-inverted commas from the clue text of non-FITB-type clues, adds those 
 	 * segments, without the quotes, to the list of clueFragments, and returns the modified clue text.
 	 * The text of FITB-type clues is handled differently, with instances of " 's " removed, and then any remaining single-inverted
 	 * commas being removed, before returning the clue text with any double-inverted commas left in place. Assumes that quotation marks
 	 * come in pairs and are not nested
-	 * This function must be called before addClueFragments is called.
-	 * @param clueText
-	 * @return
+	 * @param clueText - the text to be parsed
 	 */
 	private void parseClueText(String clueText) {
 		String textToBeFragmented = clueText;
@@ -99,51 +55,31 @@ public class ClueImpl implements Clue {
 				String sequenceAfterQuotation = textToBeFragmented.substring(indexOfEndQuote + 1, textToBeFragmented.length());
 
 				this.getClueFragments().add(quotedSequence);
-				
 				textToBeFragmented = sequenceBeforeQuotation + sequenceAfterQuotation;
-				System.err.println("textToBeFragmented after stripping quoted sequence: " + textToBeFragmented); // DEBUGGING *******
 			}
 		}
 		this.addClueFragments(textToBeFragmented);
 	}
-
-	/**
-	 * matchesStructure - see framework.Clue
-	 */
-	@Override
-	public boolean matchesStructure(Solution solution) {
-		return (Arrays.equals(solution.getSolutionStructure(), this.getSolutionStructure())); // requires comparison of deep equality
-	}
 	
 	/**
-	 * getSolutionStructureAsString - see framework.Clue
+	 * addClueFragments
+	 * @param clueText - the text of the clue to be fragmented
 	 */
-	@Override
-	public String getSolutionStructureAsString() {
-		if(this.getSolutionStructure() == null || this.getSolutionStructure().length < 1)
-			return "";
-		String structure = "[" + this.getSolutionStructure()[0];
-		for(int i = 1; i < this.getSolutionStructure().length; i++)
-			structure += ", " + this.getSolutionStructure()[i];
-		structure += "]";
-		return structure;
-	}
-	
 	private void addClueFragments(String clueText) {
 		if(this.isFillInTheBlank())
     		this.addFITBClueFragments(clueText);
 		else this.addStandardClueFragments(clueText);
 	}
 	
+	/**
+	 * addStandardClueFragments - splits the clueText argument into fragments and adds these to the clueFragments list member. Called if
+	 * the clue being constructed is of standard (i.e. non-FITB) type
+	 * @param clueText - the text of the clue to be fragmented
+	 */
     private void addStandardClueFragments(String clueText) {
     	// firstly, remove any FITB markers, since we are treating this text as not being part of a FITB sequence
     	clueText = clueText.replace(this.FILL_IN_THE_BLANK_MARKER, "");
 		String[] wordsInClueText = clueText.split(" ");
-		
-		System.out.println("Elements of wordsInClueText after splitting around space:"); // DEBUGGING **************************
-		for(int i = 0; i < wordsInClueText.length; i++) // DEBUGGING **************************
-			System.out.println(i + ": " + wordsInClueText[i]); // DEBUGGING **************************
-		
 		
 		for(int i = 0; i < wordsInClueText.length; i++) {
 			String thisWord = wordsInClueText[i];
@@ -163,17 +99,6 @@ public class ClueImpl implements Clue {
 				}
 				if(this.notSolelyPunctuation(thisWord))
 					this.getClueFragments().add(thisWord);
-				
-				/*
-				this.getClueFragments().add(thisWord);
-				// if the word ends with a comma or closing bracket, add the word without the comma/bracket as a fragment too
-				if(thisWord.length() > 1 && (thisWord.substring(thisWord.length() - 1, thisWord.length()).equals(",")
-						|| thisWord.substring(thisWord.length() - 1, thisWord.length()).equals(")")))
-					this.getClueFragments().add(thisWord.substring(0, thisWord.length() - 1));
-				// if the word begins with a (, add the word without the ( as a fragment too
-				if(thisWord.length() > 1 && thisWord.substring(0, 1).equals("("))
-					this.getClueFragments().add(thisWord.substring(1, thisWord.length()));
-				*/
 			}
 			for(int j = i + 1; j < wordsInClueText.length; j++) {
 				String wordToAppend = wordsInClueText[j];
@@ -190,18 +115,6 @@ public class ClueImpl implements Clue {
 					}
 					if(this.notSolelyPunctuation(thisWord))
 						this.getClueFragments().add(thisWord);
-					
-					
-					/*
-					this.getClueFragments().add(thisWord);
-					// if the word ends with a comma or closing bracket, add the word without the comma/bracket as a fragment too
-					if(thisWord.length() > 1 && (thisWord.substring(thisWord.length() - 1, thisWord.length()).equals(",")
-							|| thisWord.substring(thisWord.length() - 1, thisWord.length()).equals(")")))
-						this.getClueFragments().add(thisWord.substring(0, thisWord.length() - 1));
-					// if the word begins with a (, add the word without the ( as a fragment too
-					if(thisWord.length() > 1 && thisWord.substring(0, 1).equals("("))
-						this.getClueFragments().add(thisWord.substring(1, thisWord.length()));
-					*/
 				}
 			}
 		}
@@ -214,9 +127,13 @@ public class ClueImpl implements Clue {
 			String transformedClueText = clueText.replace(this.S_APOSTROPHE_SEQUENCE, "");
 			this.addStandardClueFragments(transformedClueText);
 		}
-		
 	}
     
+	/**
+	 * addFITBClueFragments - splits the clueText argument into fragments and adds these to the clueFragments list member. Called if
+	 * the clue being constructed is of FITB type
+	 * @param clueText - the text of the clue to be fragmented
+	 */
     private void addFITBClueFragments(String clueText) {
     	final String QUOTE = "\"";
     	ArrayList<String> FITBfragments = new ArrayList<String>();
@@ -228,25 +145,21 @@ public class ClueImpl implements Clue {
     	}
     	
     	while(clueText.contains(QUOTE)) {
-    		/* Clue text contains at least one double quote */
-    		/* Find the first double quote */
+    		/* Clue text contains at least one double quote - start by finding  the first double quote */
     		int indexOfStartQuote = clueText.indexOf(QUOTE);
-    		
     		if(indexOfStartQuote >= (clueText.length() - 1)) {
-    			clueText = clueText.replace(QUOTE, ""); // if no matching end-quote exists, simply strip the quote out
+    			clueText = clueText.replace(QUOTE, ""); // no matching end-quote exists, so strip the quote out
     		}
     		else {
     			/* Find a matching end-quote */
         		int indexOfEndQuote = clueText.indexOf(QUOTE, indexOfStartQuote + 1);
-        		
         		if(indexOfEndQuote == -1)
-        			clueText = clueText.replace(QUOTE, ""); // if no matching end-quote exists, simply strip the quote out
+        			clueText = clueText.replace(QUOTE, ""); // if no matching end-quote exists, strip the quote out
         		else {
         			String fragmentBeforeQuotedSequence = clueText.substring(0, indexOfStartQuote);
         			String fragmentAfterQuotedSequence = clueText.substring(indexOfEndQuote + 1, clueText.length());
         			otherFragments.add(fragmentBeforeQuotedSequence);
         			otherFragments.add(fragmentAfterQuotedSequence);
-        			
         			String FITBfragment = clueText.substring(indexOfStartQuote + 1, indexOfEndQuote);
         			FITBfragments.add(FITBfragment);
         			
@@ -255,7 +168,6 @@ public class ClueImpl implements Clue {
         		}
     		}
     	}
-    	
     	/* Parse the portions of the clue outside of quoted sections as normal */
     	for(String portionOfClue : otherFragments)
     		this.addStandardClueFragments(portionOfClue);
@@ -266,7 +178,6 @@ public class ClueImpl implements Clue {
     		 * a collection of words
     		 */
     		String[] fragmentsOfFITBsection = fragment.split(FILL_IN_THE_BLANK_MARKER);
-    		
     		for(int i = 0; i < fragmentsOfFITBsection.length; i++) {
 				String thisFragment = fragmentsOfFITBsection[i];
 				if(!this.getClueFragments().contains(thisFragment)) {
@@ -283,6 +194,14 @@ public class ClueImpl implements Clue {
     	}
     }
     
+    /**
+     * notSolelyPunctuation - the fragmentation process frequently generates clue fragments consisting of a single, common punctuation
+     * character. These need to be ignored, and the notSolelyPunctuation method provides a convenient way to check a fragment before adding
+     * it to the list of gathered fragments
+     * @param text - the String to be tested
+     * @return true if the text argument does not consist of a single punctuation element that is present in the PUNCTUATION array member,
+     * and false otherwise.
+     */
     private boolean notSolelyPunctuation(String text) {
     	for(int i = 0; i < this.PUNCTUATION.length; i++) {
     		if(this.PUNCTUATION[i].equals(text))
@@ -292,12 +211,11 @@ public class ClueImpl implements Clue {
     }
     
     /**
-     * 
-     * @param text
+     * imbalancedParentheses
+     * @param text - the String to be tested for invalid use of parentheses
      * @return true if the number of opening brackets in the text argument is not equal to the number of closing brackets
      */
     private boolean imbalancedParentheses(String text) {
-    	System.out.println("imbalancedParentheses called on: " + text); // DEBUGGING ********************************
     	final String OPEN_BRACKET = "(";
     	final String CLOSE_BRACKET = ")";
     	int openBracketCount = 0, closeBracketCount = 0;
@@ -312,9 +230,13 @@ public class ClueImpl implements Clue {
     	}
     	return (openBracketCount != closeBracketCount);
     }
-
+    
+    /**
+     * toUpperCase
+     * @param thisWord the String to be converted to upper case
+     * @return thisWord, converted to upper case
+     */
 	private String toProperCase(String thisWord) {
-		log.debug("calling substring(0,1) on the String \"" + thisWord + "\"");
 		String thisWordInProperCase = thisWord.substring(0, 1).toUpperCase();
 		if(thisWord.length() > 1) {
 			int index = 1; // start at the second letter of the word
@@ -333,9 +255,10 @@ public class ClueImpl implements Clue {
 	}
 	
 	/**
-	 * 
-	 * @param clueText
-	 * @return
+	 * imbalancedFITBMarkers - if the text of a clue contains FITB markers, then in order to construct a valid clue, 
+	 * the number of elements (i.e. words) in the solution structure must match the number of FITB markers present in the clue
+	 * @param clueText - the String to check for improper use of FITBMarkers
+	 * @return true if the number of FITB markers present in the clueText argument equals the number of elements in the solution structure
 	 */
 	private boolean imbalancedFITBMarkers(String clueText) {
 		int countOfFITBmarkers = 0;
@@ -344,5 +267,64 @@ public class ClueImpl implements Clue {
 				countOfFITBmarkers++;
 		}
 		return (countOfFITBmarkers != this.getSolutionStructure().length);
+	}
+	
+	/**
+	 * Constructor - constructs a clue from a String representing the clue text and an int[] representing the solution structure
+	 * @param clueText - the text of the clue
+	 * @param solutionStructure - an array of integers representing the structure of the solution, with each element of the array
+	 * representing a word, and the value of the element representing the number of letters in that array
+	 * @throws InvalidClueException - if either the clue text or the structure of the solution is invalid
+	 */
+	public ClueImpl(String clueText, int[] solutionStructure) throws InvalidClueException {
+		if(clueText == null || clueText.length() == 0)
+			throw new InvalidClueException("Empty clue");
+		if(solutionStructure == null || solutionStructure.length == 0)
+			throw new InvalidClueException("Invalid specification of solution structure");
+		for(int i = 0; i < solutionStructure.length; i++) {
+			if(solutionStructure[i] < 1)
+				throw new InvalidClueException("Invalid specification of solution structure");
+		}
+		this.setSolutionStructure(solutionStructure);
+		this.setClueFragments(new ArrayList<String>());
+		
+		if(clueText.contains(FILL_IN_THE_BLANK_MARKER))
+			this.setFillInTheBlank(true);
+		
+		if(this.isFillInTheBlank() && imbalancedFITBMarkers(clueText))
+			throw new InvalidClueException("The numbed of underscores in the clue text doesn't " +
+											"match the number of words in the solution");
+		this.setSourceClue(clueText);
+		this.parseClueText(clueText);
+
+		/* Logging */
+		log.debug("Clue text = " + this.getSourceClue());
+		log.debug("Clue fragments generated:");
+		for(String f : this.getClueFragments())
+			log.debug(f);
+	}
+
+	/**
+	 * matchesStructure
+	 * @Override framework.Clue.matchesStructure
+	 */
+	@Override
+	public boolean matchesStructure(Solution solution) {
+		return (Arrays.equals(solution.getSolutionStructure(), this.getSolutionStructure())); // requires comparison of deep equality
+	}
+	
+	/**
+	 * getSolutionStructureAsString
+	 * @Override framework.Clue.getSolutionStructureAsString
+	 */
+	@Override
+	public String getSolutionStructureAsString() {
+		if(this.getSolutionStructure() == null || this.getSolutionStructure().length < 1)
+			return "";
+		String structure = "[" + this.getSolutionStructure()[0];
+		for(int i = 1; i < this.getSolutionStructure().length; i++)
+			structure += ", " + this.getSolutionStructure()[i];
+		structure += "]";
+		return structure;
 	}
 }
