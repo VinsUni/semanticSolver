0402d3cd4d160b75dea0f071b38baf4fa4976853
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 21:14:51 +0100

    tidying and commenting NSPrefixLoader class

--- src/app/NsPrefixLoader.java
+++ src/app/NsPrefixLoader.java
@@ -3,27 +3,31 @@ package app;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 import com.hp.hpl.jena.rdf.model.Model;
 
+/**
+ * @author Ben Griffiths
+ * NsPrefixLoader
+ * Utility class used to load a set of standard namespace prefixes, as used on DBpedia, into an instance of com.hp.hpl.jena.rdf.model.Model
+ */
 public class NsPrefixLoader {
-	public Model model;
 	private final String STANDARD_PREFIXES = "standardNsPrefixes.txt";
+	@Getter(AccessLevel.PRIVATE) @Setter(AccessLevel.PRIVATE) private Model model;
 	
-	
+	/**
+	 * Constructor - instantiate a new NsPrefixLoader with the given model
+	 * @param model - an instance of com.hp.hpl.jena.rdf.model.Model into which prefixes are to be loaded
+	 */
 	public NsPrefixLoader(Model model) {
 		this.setModel(model);
 	}
-	
-	private Model getModel() {
-		return this.model;
-	}
-	
-	private void setModel(Model model) {
-		this.model = model;	
-	}
 
 	/**
-	 * Load the file containing standard namespace prefixes from http://dbpedia.org/sparql?nsdecl and apply each
+	 * loadStandardPrefixes - loads the file containing standard namespace prefixes from http://dbpedia.org/sparql?nsdecl and applies each
 	 * prefix to the model member
 	 */
 	public void loadStandardPrefixes() {
