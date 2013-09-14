2b0044763dcf83e25d3313ba4d3f95428a441f4c
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 14:09:01 +0100

    tidying comments in CandidateSelector

--- src/app/CandidateSelector.java
+++ src/app/CandidateSelector.java
@@ -10,15 +10,15 @@ import framework.remotePrototype.Pop;
 
 /**
  * CandidateSelector
- * A simple extension of Jena's built-in implementation of the Selector interface, this class is used as a convenient method
- * of selecting all Statements in a model in which the predicate is Pop.relationalProperty
+ * A simple extension of Jena's built-in implementation of the Selector interface, this class is used as a convenient means
+ * of selecting all Statements in a model in which the predicate is not pop:relationalProperty
  * @author Ben Griffiths
  * @extends com.hp.hpl.jena.rdf.model.SimpleSelector
  */
 public class CandidateSelector extends SimpleSelector {
 	
 	/**
-	 * Default constructor. Constructs a new CandidateSelector with the specified arguments
+	 * Default constructor - constructs a new CandidateSelector with the specified arguments
 	 * @param subject - the subject to be matched by the CandidateSelector
 	 * @param predicate - the predicate to be matched by the CandidateSelector
 	 * @param object - the object to be matched by the CandidateSelector
@@ -29,9 +29,10 @@ public class CandidateSelector extends SimpleSelector {
 	
 	/**
 	 * selects - returns true if the statement should be selected by this CandidateSelector, otherwise false.
-	 * See com.hp.hpl.jena.rdf.model.SimpleSelecto;.
+	 * See com.hp.hpl.jena.rdf.model.SimpleSelector;.
+	 * @override @override com.hp.hpl.jena.rdf.model.Selector.selects
 	 * @argument statement - the statement to be tested
-	 * @return false for all statements whose predicate is a sub-property of pop:relationalProperty, and true for all others
+	 * @return false for all statements whose predicate is pop:relationalProperty, and true for all others
 	 */
 	@Override
 	public boolean selects(Statement statement) {
@@ -43,10 +44,10 @@ public class CandidateSelector extends SimpleSelector {
 	}
 	
 	/**
-	 * isSimple - used by the Jena framework to ascertain whether or not the selects statement has been overriden for this 
+	 * isSimple - used by the Jena framework to ascertain whether or not the selects statement has been overridden for this 
 	 * implementation of the Selector interface. See com.hp.hpl.jena.rdf.model.SimpleSelector.
-	 * @return false, since the selects method has been overwritten.
-	 * 
+	 * @override com.hp.hpl.jena.rdf.model.Selector.isSimple
+	 * @return false, since the selects method has been overridden.
 	 */
 	@Override
 	public boolean isSimple() {
