534c9b8b0ad5cb82fd419aee4ce3ee78abc473a4
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 15:41:26 +0100

    commenting SemanticSolver interface

--- src/framework/SemanticSolver.java
+++ src/framework/SemanticSolver.java
@@ -19,13 +19,15 @@ public interface SemanticSolver {
 	public void solve(Clue clue) throws QueryExceptionHTTP;
 	
 	/**
-	 * 
-	 * @param recognisedResourceUris
+	 * findSolutions - used to instruct the SemanticSolver object to begin solving a new clue after Entity Recognition has taken place.
+	 * This method relies on solve having been called first, but is provided to allow the UserSelectionBasedUserInterface (a prototype GUI
+	 * class) to gather user feedback after Entity Recognition has taken place
+	 * @param recognisedResourceUris - the list of recognised entities in the text of the clue to be solved
 	 */
 	public void findSolutions(ArrayList<String> recognisedResourceUris);
 	
 	/**
-	 * 
+	 * persistKnowledgeBase - used to instruct the SemanticSolver object to save newly gathered solutions to clues to disk
 	 */
 	public void persistKnowledgeBase();
 }
