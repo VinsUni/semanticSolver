0c215f0f96ef443661dc7f620d0d66d175d9d32e
Ben Griffiths <benstephengriffiths@gmail.com>
2013-09-14 21:04:53 +0100

    tidying and commenting ModelLoader class

--- src/app/ModelLoader.java
+++ src/app/ModelLoader.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package app;
 
 import java.io.InputStream;
@@ -23,7 +20,9 @@ import lombok.Setter;
 
 /**
  * @author Ben Griffiths
- *
+ * ModelLoader
+ * Utility class providing static methods to load a single instance of each of the pop ontology, the crossword knowledge base, and a
+ * list of the most common clue fragments, and provide references to the unique instances of each of these data sources
  */
 public class ModelLoader {
 	private static Logger log = Logger.getLogger(ModelLoader.class);
@@ -32,15 +31,18 @@ public class ModelLoader {
 	@Setter(AccessLevel.PRIVATE) private static Map<String, Boolean> commonClueFragments;
 	private static final String COMMON_CLUE_FRAGMENTS_FILE_LOCATION = "commonClueFragments.txt";
 	
+	/**
+	 * The only constructor is private; the ModelLoader class is not designed to be instantiated
+	 */
 	private ModelLoader() {
-		/* the only constructor is private */
+		
 	}
 	
 	/**
 	 * getModel - when first called, this method instantiates a Model by loading the Pop ontology from a file. It also loads a set of 
 	 * standard namespace prefixes into the Model, from another file, and then returns the model. The Model member is treated as a 
 	 * Singleton and thus subsequent calls to the method result in the Model simply being returned. 
-	 * @return - the unique static instance of Model that is a member of the ModelLoader class
+	 * @return - the unique static instance of com.hp.hpl.jena.rdf.model.Model that is the model member of the ModelLoader class
 	 */
 	public static Model getModel() {
 		if(model == null) {
@@ -56,12 +58,12 @@ public class ModelLoader {
 	 * getKnowledgeBase - when first called, this method instantiates a basic Model with the content of the RDF file that stores the 
 	 * crossword knowledge base on disk, and returns the model. The Model member is treated as a Singleton and thus subsequent calls 
 	 * to the method result in the Model simply being returned. 
-	 * @return - the unique static instance of a Model that is a member of the ModelLoader class
+	 * @return - the unique static instance of a com.hp.hpl.jena.rdf.model.Model that is the knowledgeBase member of the ModelLoader class
 	 */
 	public static Model getKnowledgeBase() {
 		if(knowledgeBase == null) {
 			try {
-				setKnowledgeBase(FileManager.get().loadModel(CrosswordKB.LOCAL_KNOWLEDGE_BASE_URI)); // Read the KB into a model
+				setKnowledgeBase(FileManager.get().loadModel(CrosswordKB.LOCAL_KNOWLEDGE_BASE_URI));
 			}
 			catch(JenaException e) {
 				log.debug("Failed to load " + CrosswordKB.LOCAL_KNOWLEDGE_BASE_URI);
@@ -71,6 +73,12 @@ public class ModelLoader {
 		return knowledgeBase;
 	}
 	
+	/**
+	 * getCommonClueFragments - when first called, instantiates a map containing all of the most common clue fragments by loading the
+	 * content of the commonClueFragments text file into memory. Each map entry is given a value of true; a map is used in order to
+	 * provide fast random access
+	 * @return the unique static instance of java.util.HashMap containing all of the most common clue fragments
+	 */
 	public static Map<String, Boolean> getCommonClueFragments() {
 		if(commonClueFragments == null) {
 			setCommonClueFragments(new HashMap<String, Boolean>());
